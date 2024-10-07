5f5ad597567bc93b1ff14651d389c70643cf67df
Kesavan Kushalnagar <kkushalnagar@mbp-kkus2.corp.carbon3d.com>
2024-10-06 19:10:03 -0700

    level 3 started

--- hud.gd
+++ hud.gd
@@ -6,12 +6,18 @@ func _ready() -> void:
 	$NextLevelButton.hide()
 	$SuccessLabel.hide()
 
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("level_one"):
+		get_tree().change_scene_to_file("res://level_1.tscn");
+	if Input.is_action_pressed("level_two"):
+		get_tree().change_scene_to_file("res://level_2.tscn");
+	if Input.is_action_pressed("level_three"):
+		get_tree().change_scene_to_file("res://level_3.tscn");
+	
 func level_finished() -> void:
 	$NextLevelButton.show();
 	$SuccessLabel.show();
 
 func _on_next_level_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level 2.tscn");
+	get_tree().change_scene_to_file("res://level_2.tscn");
