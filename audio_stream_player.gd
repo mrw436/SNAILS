19b093ed7815ec121686389b09df4f1de70fbf1b
Maddie Wilkinson <mw350@duke.edu>
2024-10-19 14:58:06 -0700

    chanigng mole size commenting out sound

--- audio_stream_player.gd
+++ audio_stream_player.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if !playing:
-		play();
+		#play();
 	pass
